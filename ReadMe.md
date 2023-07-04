13d8983cd5c7593960f8686991958e7d503f0f64
mehrdad-zade <mehrdad.azh@gmail.com>
2023-07-04 16:44:37 -0400

    refactor

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,12 @@
+### Get Setup
+
+python==3.9.7
+
+using ```pipreq .``` generated required installations for python libraries used
+```
+pip install -r requirements.txt
+```
+
 ### Run the application
 
 - Open two terminals, one for the backend and one for the frontend
--- backend/openai_api.py
+++ backend/openai_api.py
@@ -6,7 +6,7 @@ pip install transformers
 
 import os
 import openai
-from gpt_index import SimpleDirectoryReader, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from llama_index import SimpleDirectoryReader, GPTVectorStoreIndex, LLMPredictor, PromptHelper
 from langchain import OpenAI
 from llama_index import GPTVectorStoreIndex, TrafilaturaWebReader
 import chromadb
@@ -35,7 +35,7 @@ def createVectorIndex():
     docs = SimpleDirectoryReader(LOCAL_PATH + 'data/source_of_knowledge').load_data() # make sure only the text files are in the folder
 
         
-    vectorIndex = GPTSimpleVectorIndex(documents=docs, llm_predictor=llmPredictor, prompt_helper=prompt_helper)
+    vectorIndex = GPTVectorStoreIndex(documents=docs, llm_predictor=llmPredictor, prompt_helper=prompt_helper)
     vectorIndex.save_to_disk(vIdx)
 
     return vectorIndex
@@ -77,10 +77,10 @@ def chatGPT3_response(user_input):
         )
     return res["choices"][0]["message"]["content"]
 
-# Example usage
-prompt = "What is the capital of France?"
-response = chatGPT3_response(prompt)
-print(response)
+# # Example usage
+# prompt = "What is the capital of France?"
+# response = chatGPT3_response(prompt)
+# print(response)
 
 
 def create_embedding(name):
@@ -96,12 +96,12 @@ def query_pages(collection, urls, questions):
         print(f"Answer: {query_engine.query(question)}")
 
 
-# # test case
-# urls = ["https://mehrdad-zade.github.io/", "https://mehrdad-zade.github.io/#Experience"]    
-# questions = ["tell me a bit about mehrdad's background what kind of job would he be suitable for"]
-# collection = create_embedding("supertype")
-# query_pages(
-#         collection,
-#         urls,
-#         questions
-#     )
+# test case
+urls = ["https://mehrdad-zade.github.io/", "https://mehrdad-zade.github.io/#Experience"]    
+questions = ["tell me a bit about mehrdad's background what kind of job would he be suitable for"]
+collection = create_embedding("supertype")
+query_pages(
+        collection,
+        urls,
+        questions
+    )
